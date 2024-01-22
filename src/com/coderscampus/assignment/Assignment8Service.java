c6576ae1c0476437bc2652090dc2a24f41a56fd2
caijda <145417979+caijda@users.noreply.github.com>
2024-01-21 21:22:19 -0700

    Finished Assignment

--- src/com/coderscampus/assignment/Assignment8Service.java
+++ src/com/coderscampus/assignment/Assignment8Service.java
@@ -1,6 +1,36 @@
 package com.coderscampus.assignment;
 
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Assignment8Service {
     Assignment8 assignment = new Assignment8();
+    ExecutorService cashedService = Executors.newCachedThreadPool();
+    ExecutorService singleService = Executors.newSingleThreadScheduledExecutor();
+    List<Integer> numbersList = new ArrayList<>();
 
+    public void getData() {
+        for (int i = 0; i < 1000; i++) {
+            CompletableFuture.supplyAsync(assignment::getNumbers, cashedService)
+                    .thenAcceptAsync(list -> numbersList.addAll(list), singleService);
+        }
+    }
+
+    public void unique() {
+        Map<Integer, Long> map = new LinkedHashMap<>();
+        for (int i = 0; i < 15; i++) {
+            int finalI = i;
+            map.put(i, numbersList.stream()
+                    .filter(n -> n == finalI)
+                    .count());
+        }
+        System.out.println("");
+        System.out.println("Values:");
+        System.out.println(map);
+        cashedService.shutdown();
+        singleService.shutdown();
+    }
 }
+
--- src/com/coderscampus/assignment/Main.java
+++ src/com/coderscampus/assignment/Main.java
@@ -2,7 +2,12 @@ package com.coderscampus.assignment;
 
 public class Main {
     public static void main(String[] args) {
-    Assignment8Service service = new Assignment8Service();
+        Assignment8Service service = new Assignment8Service();
 
+        service.getData();
+        while (service.numbersList.size() != 1000000) {
+            System.out.println(service.numbersList.size());
+        }
+        service.unique();
     }
 }
